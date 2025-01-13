d00718f721ba4361bdd523bb9228b3a60f9900f3
felsapvs <profelipealves@outlook.com>
2025-01-13 15:27:37 -0300

    Refactor routing in App component to include authentication and protect routes; replace Auth component with LoginPage.

--- src/App.tsx
+++ src/App.tsx
@@ -3,21 +3,43 @@ import { BrowserRouter, Routes, Route, Navigate } from "react-router-dom";
 import Layout from "./components/Layout";
 import Sales from "./pages/Sales";
 import Products from "./pages/Products";
-import Auth from "./components/Auth";
+import Auth from "./components/LoginPage";
+import { AuthProvider } from "./context/AuthProvider";
+import { ProtectedRoute } from "./ProtectedRoute";
+import LoginPage from "./components/LoginPage";
+import { ProductGrid } from "./components/sales/ProductGrid";
 
 function App() {
   return (
     <BrowserRouter>
-      <Layout>
-        <Routes>
-          <Route path="/" element={<Navigate to="/sales" replace />} />
-          <Route path="/sales" element={<Sales />} />
-          <Route path="/products" element={<Products />} />
-          <Route path="auth" element={<Auth />} />
-          <Route path="/reports" element={<div>Em desenvolvimento</div>} />
-          <Route path="/settings" element={<div>Em desenvolvimento</div>} />
-        </Routes>
-      </Layout>
+      <AuthProvider>
+        <Layout>
+          <Routes>
+            <Route path="/login" element={
+              <ProtectedRoute requireAuth={false}>
+                <LoginPage />
+              </ProtectedRoute>
+            } />
+            <Route path="/" element={<ProtectedRoute><Sales /></ProtectedRoute>} />
+            <Route path="/sales" element={<ProtectedRoute><Sales /></ProtectedRoute>} />
+            <Route
+              path="/products"
+              element={<ProtectedRoute><Products /></ProtectedRoute>}
+            />
+            {/* <Route path="/products" element={<ProtectedRoute><Products /></ProtectedRoute>} /> */}
+            <Route
+              path="*"
+              element={
+                <div className="min-h-screen flex items-center justify-center">
+                  <h1 className="text-2xl font-bold">Página não encontrada</h1>
+                </div>
+              }
+            />
+            <Route path="/reports" element={<div>Em desenvolvimento</div>} />
+            <Route path="/settings" element={<div>Em desenvolvimento</div>} />
+          </Routes>
+        </Layout>
+      </AuthProvider>
     </BrowserRouter>
   );
 }
